cea6d8906f74f06acaffaa0b41e9dd5e6d68049d
Imanda03 <asis03ktm@gmail.com>
2023-04-17 16:49:53 +0545

    verify token

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ export const login = async (req, res, next) => {
     const { password, isAdmin, ...otherDetails } = user._doc;
 
     res
-      .cookie("access_tooken", token, { httpOnly: true })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .send({ ...otherDetails });
   } catch (error) {
--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,13 @@ import {
   getSingleUser,
   updateUser,
 } from "../controllers/userController.js";
+import { verifyToken } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkAuthentication", verifyToken, (req, res, next) => {
+  res.send("Hello user You are logged in");
+});
 
 //UPDATE
 router.put("/:id", updateUser);
