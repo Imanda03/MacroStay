f8232c43b0a6228d900d977a1d9bba6c846c9a55
Imanda03 <asis03ktm@gmail.com>
2023-05-08 21:35:08 +0545

    frontend

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8081/api",
+      changeOrigin: true,
+    })
+  );
+};
