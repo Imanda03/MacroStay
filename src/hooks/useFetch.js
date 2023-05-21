2812eaffa42c3e4c77cda006f6c8edd3f916ea3f
Imanda03 <asis03ktm@gmail.com>
2023-05-21 23:02:35 +0545

    admin pannel

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (error) {
+        setError(error);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+  return { data, loading, error, reFetch };
+};
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,76 @@
+import { useState, useContext } from "react";
+import "./login.scss";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [credentials, setCardentials] = useState({
+    username: undefined,
+    password: undefined,
+  });
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setCardentials((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "LOGIN_START",
+    });
+    try {
+      const res = await axios.post(
+        "http://localhost:8081/api/auth/login",
+        credentials
+      );
+      if (res.data.isAdmin) {
+        dispatch({
+          type: "LOGIN_SUCCESS",
+          payload: res.data.details,
+        });
+        navigate("/");
+      } else {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { message: "You are not allowed" },
+        });
+      }
+    } catch (error) {
+      dispatch({ type: "LOGIN_FAILURE", payload: error.response.data });
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="lContainer">
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          name="email"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          className="lInput"
+        />
+        <button disabled={loading} className="lButton" onClick={handleClick}>
+          Login
+        </button>
+        {error && <span>{error.message}</span>}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
