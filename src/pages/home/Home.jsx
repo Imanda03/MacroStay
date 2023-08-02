ad720dae827aa7d35d4f5fd100b24af405c078ee
Imanda03 <asis03ktm@gmail.com>
2023-08-02 15:11:46 +0545

    some change in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,20 +5,37 @@ import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
 // import Table from "../../components/table/Table";
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from "@material-ui/core";
-
- 
-function createData(userName, hotel, room, days, price){
-  return {userName, hotel, room, days, price};
-}
-
-const rows =[
-  createData("Anish", "kathmandu Hotel", "201", 5, 2000) ,
- createData("Mandish", "Pokhara Hotel", "301", 5, 2000) ,
-  createData("Isha", "Chitwan Hotel", "401", 5, 5000) 
-]
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@material-ui/core";
+import { useFetch } from "../../hooks/useFetch";
+import { useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [userid, setUserid] = useState("");
+  const [reserveid, setReserveid] = useState("");
+  const { data, loading, error } = useFetch(
+    `http://localhost:8081/api/reserve`
+  );
+  const handleClick = async () => {
+    try {
+      await axios.delete(
+        `http://localhost:8081/api/reserve/${reserveid}/${userid}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log("user : " + userid);
+  console.log("reserve : " + reserveid);
   return (
     <div className="home">
       <Sidebar />
@@ -29,30 +46,43 @@ const Home = () => {
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
-                <TableCell align="center" >Name</TableCell>
+                <TableCell align="center">UserName</TableCell>
                 <TableCell align="center">Hotel</TableCell>
                 <TableCell align="center">Room.No</TableCell>
                 <TableCell align="center">Days</TableCell>
                 <TableCell align="center">Price</TableCell>
+                <TableCell align="center">Action</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) =>(
-                <TableRow key={row.number}>
+              {data.map((item) => (
+                <TableRow key={item._id}>
                   <TableCell component="th" scope="row" align="center">
-                    {row.userName}
+                    {item.userName}
                   </TableCell>
-                  <TableCell align="center">{row.hotel}</TableCell>
-                  <TableCell align="center">{row.room}</TableCell>
-                  <TableCell align="center">{row.days} days</TableCell>
-                  <TableCell align="center">Rs.{row.price}</TableCell>
+                  <TableCell align="center">{item.hotelName}</TableCell>
+                  <TableCell align="center">{item.roomNumber}</TableCell>
+                  <TableCell align="center">{item.days} days</TableCell>
+                  <TableCell align="center">Rs.{item.price}</TableCell>
+                  <TableCell align="center">
+                    <Button
+                      variant="contained"
+                      color="warning"
+                      size="small"
+                      onClick={
+                        ((() => () => setUserid(item.userid),
+                        () => setReserveid(item._id)),
+                        () => handleClick())
+                      }
+                    >
+                      Delete
+                    </Button>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
           </Table>
         </TableContainer>
-
-
 
         {/* <div className="widgets">
           <Widget type="user" />
@@ -68,7 +98,6 @@ const Home = () => {
           <div className="listTitle">Latest Transactions</div>
           <Table />
         </div> */}
-        
       </div>
     </div>
   );
